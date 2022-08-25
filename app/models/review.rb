c62b8b6857fb2f939d1276d3676bbdc71bd88ffc
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-24 19:17:55 -0500

    Added numericality and inclusion validations to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,10 @@
 class Review < ApplicationRecord
   # Validations
   enum context: { consulted: 0, hired: 1 }
-  validates :rating, presence: true
+  validates :rating, presence: true,
+                     numericality: { only_integer: true },
+                     inclusion: { in: 1..5 }
+
   validates :title, presence: true
   validates :content, presence: true
   validates :context, presence: true
