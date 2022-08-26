78ac2d8ddad482914cf43d9e064b843d65d056ab
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-25 19:31:15 -0500

    API configured the user stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      user.regenerate_token
+      respond_user = {
+        name: user.name,
+        email: user.email
+        token: user.token
+      }
+      render json: respond_user, status: :ok
+    else
+      respond_unauthorized('Incorrect email or password')
+    end
+  end
+
+  def destroy
+    current_user.invalidate_token
+    head :ok
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      render @user.to_json(except: [:password_digest]), status: :created
+    else
+      render json: { errors: @user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  
+  def show
+    render json: @respond_user
+  end
+  
+  def update
+    if current_user.update(user_params)
+      respond_user = {
+        name: current_user.name,
+        email: current_user.email
+      }
+      render json: respond_user, :status_ok
+    else
+      render json: {errors: current_user.errors }, status: :unprocessable_entity
+  end
+  
+  def destroy
+    current_user.destroy
+    head :ok
+  end
+  
+  
+  private
+  
+  def set_user
+    @respond_user = {
+      name: current_user.name,
+      email: current_user.email,
+    }
+  end
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
 end
