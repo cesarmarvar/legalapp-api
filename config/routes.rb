94b8a9c1648ebb2a83c12b5d13bd7588519fb420
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-30 13:12:56 -0500

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,36 @@
 Rails.application.routes.draw do
+  # Sessions:
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
+  
+  # Users:
+  resources :users
+  post '/profile', to: 'users#create'
+  patch '/profile/edit', to: 'users#update'
+  delete '/profile', to: 'users#destroy'
+  get '/profile', to: 'users#show'
+
+  # Reviews:
+  post '/reviews/new', to: 'reviews#create'
+  get '/reviews/lawyer/:id', to: 'reviews#index'
+  get '/review/:id', to: 'reviews#show'
+  delete 'review/:id', to: 'reviews#destroy'
+
+  # Lawyers:
+  resources :lawyers 
+  post '/lawyers/new' => 'lawyers#create'
+
+  # Questions:
+  get '/questions', to: 'questions#index'
+  post '/questions/new', to: 'questions#create'
+  get '/questions/user/:id', to: 'questions#get_user_questions'
+  get '/question/:id', to: 'questions#show'
+  get '/question/answers/:id', to: 'questions#get_question_answers'
+
+  # Answers
+  post 'answers/new', to: 'answers#create'
+  get 'answers', to: 'answers#index'
+  get 'answer/:id', to: 'answers#show'
+  get 'answers/lawyer/:id', to: 'answers#get_lawyer_answers'
 
 end
