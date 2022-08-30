a2a5f3d98a8e2d99b9b5e9ee9cf100b440fd7691
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-30 13:12:33 -0500

    Corrected review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,15 +1,12 @@
 class Review < ApplicationRecord
   # Validations
   enum context: { consulted: 0, hired: 1 }
-  validates :rating, presence: true,
-                     numericality: { only_integer: true },
-                     inclusion: { in: 1..5 }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
 
   validates :title, presence: true
   validates :content, presence: true
   validates :context, presence: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP } # deberia condicionarse a si no hay un user loggeado? 
 
   # Associations
   belongs_to :lawyer
