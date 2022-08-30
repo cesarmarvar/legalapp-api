f2b447903b5ca04c821cba08e79bc08766360c14
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-30 13:13:04 -0500

    Minor fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_000347) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_234710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,16 +49,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_000347) do
     t.integer "long"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_lawyers_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
     t.string "question"
     t.text "explanation"
-    t.bigint "user_id", null: false
-    t.bigint "lawyer_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["lawyer_id"], name: "index_questions_on_lawyer_id"
+    t.string "confirmation"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
@@ -70,7 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_000347) do
     t.string "name"
     t.string "email"
     t.bigint "lawyer_id", null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["lawyer_id"], name: "index_reviews_on_lawyer_id"
@@ -90,14 +91,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_000347) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "token"
-    t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
   add_foreign_key "answers", "lawyers"
   add_foreign_key "answers", "questions"
   add_foreign_key "lawyer_specialities", "lawyers"
   add_foreign_key "lawyer_specialities", "specialities"
-  add_foreign_key "questions", "lawyers"
+  add_foreign_key "lawyers", "users"
   add_foreign_key "questions", "users"
   add_foreign_key "reviews", "lawyers"
   add_foreign_key "reviews", "users"
