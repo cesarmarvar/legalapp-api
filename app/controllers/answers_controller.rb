fd96d5a7d7236b2b6856275609e57f9e53c4ca11
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-30 13:10:28 -0500

    Set up answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,53 @@
 class AnswersController < ApplicationController
+  skip_before_action :authorize, only: %i[index show get_lawyer_answers]
+  before_action :set_answer, only: %i[show]
+
+  def index 
+    @answers = Answer.all
+    render json: @answers
+  end
+
+  def create 
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      render json: @answer, status: :created
+    else
+      render json: { errors: @answer.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    if @answer
+      render json: @answer
+    else
+      render json: [], status: :not_found
+    end
+  end
+
+  def get_lawyer_answers
+    answers = Answer.where(lawyer_id: params[:id])
+    if answers
+      render json: answers
+    else
+      render json: [], status: :not_found
+    end
+  end
+  
+
+  private
+
+  def set_answer
+    data = Answer.find(params[:id])
+    @answer = {
+      id: data.id,
+      answer: data.answer,
+      lawyer_id: data.lawyer_id
+    }
+  end
+
+  def answer_params
+    params.permit(:answer, :question_id, :lawyer_id)
+  end
+
+
 end
