13e127cad6278421fd8057445cc7dd804a60de45
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-30 13:10:59 -0500

    Set up reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,68 @@
 class ReviewsController < ApplicationController
+  skip_before_action :authorize, only: %i[ index create show]
+  before_action :set_review, only: %i[ show destroy ]
+
+  # Shows a specific lawyer's reviews:
+  def index
+    reviews = Review.where(lawyer_id: params[:id])
+    if reviews
+      render json: reviews
+    else
+      render json: [], status: :not_found
+    end
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      render json: @review, status: :created
+    else
+      render json: { errors: @review.errors }, status: :unprocessable_entity
+    end
+  end
+
+  # Shows a single review's detail:
+  def show
+    if @review
+      render json: @review
+    else
+      render json: [], status: :not_found
+    end
+  end
+
+  def destroy
+    @review.destroy
+    head :ok
+  end
+
+  private
+  
+  def set_review
+    data = Review.find(params[:id])
+    @review = {
+      id: data.id,
+      rating: data.rating,
+      title: data.title,
+      content: data.content,
+      context: data.context,
+      name: data.name,
+      email: data.email,
+      lawyer_id: data.lawyer_id,
+      user_id: data.user_id
+    }
+  end
+
+  def review_params
+    params.permit(
+    :rating, 
+    :title, 
+    :content, 
+    :context, 
+    :name, 
+    :email, 
+    :lawyer_id, 
+    :user_id 
+    )
+  end
+
 end
