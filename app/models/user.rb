3987c317ef7580eeee8ea8853f944f1da1da8db0
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-30 13:12:49 -0500

    Corrected lawyer model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Validations
   validates :email, presence: true, 
             uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true
             # format: { with: /^(?=.*\d)(?=.*([a-z]|[A-Z]))([\x20-\x7E]){8,}$/ }
 
   # Associations
@@ -10,6 +9,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :questions
   has_many :reviews
+  has_one :lawyer
 
   def invalidate_token
     update(token: nil)
