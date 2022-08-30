a2985a0e1a417344437c5a183ede4792e5069f21
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-30 13:10:20 -0500

    Set up questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,54 @@
 class QuestionsController < ApplicationController
+  skip_before_action :authorize, only: %i[index show get_user_questions get_question_answers]
+
+  def index
+    @questions = Question.all.order(:id)
+    if @questions
+      render json: @questions
+    else
+      render json: [], status: :not_found
+    end
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      render json: @question, status: :created
+    else
+      render json: { errors: @question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    if @question 
+      render json: @question, status: :ok
+    else
+      render json: [], status: :not_found
+    end
+  end
+
+  def get_user_questions
+    questions = Question.where(user_id: params[:id])
+    if questions
+      render json: questions
+    else
+      render json: [], status: :not_found
+    end
+  end 
+
+  def get_question_answers
+    question = Question.find(params[:id])
+    if question.answers
+      render json: question.answers, status: :ok
+    else
+      render json: [], stats: :not_found
+    end
+  end
+
+  private
+
+  def question_params
+    params.permit(:question, :explanation, :confirmation, :user_id)
+  end
 end
