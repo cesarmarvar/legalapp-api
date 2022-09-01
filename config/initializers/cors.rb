bc92071d6de960275bf7c99b542d33b6d9786288
Cesar Martinez <cmarvarc@hotmail.com>
2022-08-31 21:13:07 -0500

    Added localhost to cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://legalapp-0822.herokuapp.com/'
+    origins 'https://legalapp-0822.herokuapp.com/', 'http://localhost:3000'
     resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
 end
