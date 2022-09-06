a8bd22131c4d912df8ec3470e58bc8fdf21acf54
Cesar Martinez <cmarvarc@hotmail.com>
2022-09-06 17:29:49 -0500

    Added create method on photos controller. Testeable with Cloudinary

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,15 @@
 class PhotosController < ApplicationController
 
+  def create
+    result = Cloudinary::Uploader.upload(params[:image])
+
+    photo = Photo.create(photoable_id: params[:photoable_id], image: result['url'])
+
+    if photo.save
+      render json: photo
+    else
+      render json: { errors: photo.errors }, status: :unprocessable_entity
+    end
+  end
 
 end
