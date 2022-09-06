f68a426f44a34f7b021ab55e7f859a5617933e7b
Cesar Martinez <cmarvarc@hotmail.com>
2022-09-06 10:41:27 -0500

    cloudinary setting

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = "dey9nridw"
+  config.api_key = "483528277395175"
+  config.api_secret = "LNSxbLnmqFxmj0VrVS-kvI-El18"
+  config.secure = true
+  config.cdn_subdomain = true
+end
