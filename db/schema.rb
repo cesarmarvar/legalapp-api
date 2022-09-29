72d2cfde1d94553f6f5bd168ad1d3134c8089301
Cesar Martinez <cmarvarc@hotmail.com>
2022-09-29 10:42:51 -0500

    renamed user column user to username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_161825) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_145339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -124,7 +124,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_161825) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
