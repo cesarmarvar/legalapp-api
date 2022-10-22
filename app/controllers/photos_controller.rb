162ae4209b94ade2385ed7992a1f51da55cf0d1e
emachipana <emachipana0102@gmail.com>
2022-10-21 19:33:52 -0500

    FIX: create on photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,15 +1,19 @@
 class PhotosController < ApplicationController
 
   def create
-    result = Cloudinary::Uploader.upload(params[:image])
-
-    photo = Photo.create(photoable_id: params[:photoable_id], image: result['url'])
+    photo = Photo.new(photo_params)
 
     if photo.save
       render json: photo
     else
       render json: { errors: photo.errors }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def photo_params
+    params.permit(:image, :photoable_id, :photoable_type)
   end
 
 end
