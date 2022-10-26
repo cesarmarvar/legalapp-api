2744b4b8a46c22296e1289bb588f1bd5c1089285
Cesar Martinez <cmarvarc@hotmail.com>
2022-10-25 21:07:00 -0500

    Added update method for Photo controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,6 +10,15 @@ class PhotosController < ApplicationController
     end
   end
 
+  def update
+    photo = Photo.find(params[:id])
+    if photo.update(photo_params)
+      render json: photo, status: :ok
+    else
+      render json: { errors: photo.errors }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def photo_params
