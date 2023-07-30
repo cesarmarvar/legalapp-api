3672a6dac79ccb70f29f645657a7b4a8521239d1
Cesar Martinez <cmarvarc@hotmail.com>
2023-07-30 16:14:43 -0500

    Added new domain to cors (railway)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://legalapp-0822.herokuapp.com/', 'http://localhost:3000'
+    # origins 'https://legalapp-0822.herokuapp.com/', 'http://localhost:3000'
+    origins 'legalapp-api-production-0424.up.railway.app/', 'http://localhost:3000'
     resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
 end
