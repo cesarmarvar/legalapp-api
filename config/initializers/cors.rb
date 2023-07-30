f3db615e3d8676db4d31bdd069c1692df2e34c83
Cesar Martinez <cmarvarc@hotmail.com>
2023-07-30 16:21:31 -0500

    Modified cors to include both heroku and railway domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://legalapp-0822.herokuapp.com/', 'http://localhost:3000'
-    origins 'legalapp-api-production-0424.up.railway.app/', 'http://localhost:3000'
+    origins 'https://legalapp-0822.herokuapp.com/', 'http://localhost:3000', 'https://legalapp-api-production-0424.up.railway.app/'
     resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
 end
